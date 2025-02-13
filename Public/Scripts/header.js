f261f29ad0cfe8681cfb50cd7cac5ef59770ef43
Daniel Ferreira <pocketrocket@outlook.com.br>
2025-02-13 00:00:24 -0300

    Modal de registro adicionado

--- Public/Scripts/header.js
+++ Public/Scripts/header.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    // Criação do modal
-    const modalElement = `
-    <dialog>
-        <div id="form-login">
-            <button id="close"><img src="../Assets/close.png" alt=""></button>
+    // Criação do modal login
+    const modalElementLogin = `
+    <dialog id="dialog-login">
+        <div class="form-modal-ls">
+            <button id="close-login" class="close"><img src="../Assets/close.png" alt=""></button>
             <h3>Log in</h3>
     
             <form action="">
@@ -21,23 +21,55 @@ document.addEventListener("DOMContentLoaded", () => {
                 <input type="password" placeholder="Senha">
             </form>
     
-            <button id="login-submit">ENTRAR</button>
-            <button id="signin">Registre-se</button>
+            <button id="login-submit" class="modal-submit">ENTRAR</button>
+            <button id="signin-button">Registre-se</button>
         </div>
     </dialog>
     `
-    document.body.insertAdjacentHTML("beforeend", modalElement);
+    document.body.insertAdjacentHTML("beforeend", modalElementLogin);
 
     // Lógica de abertura e fechamento do modal de login
-    const modalLogin = document.querySelector('dialog')
+    const modalLogin = document.querySelector('#dialog-login')
 
     const buttonLogin = document.querySelector('#loginButton')
     buttonLogin.onclick = function () {
         modalLogin.showModal()
+
+        const buttonClose = document.querySelector('#close-login')
+        buttonClose.onclick = function () {
+            modalLogin.close()
+        }
     }
 
-    const buttonClose = document.querySelector('#close')
-    buttonClose.onclick = function () {
+    // Criação do modal signin
+    const modalElementSignin = `
+    <dialog id="dialog-signin">
+        <div class="form-modal-ls">
+            <button id="close-signin" class="close"><img src="../Assets/close.png" alt=""></button>
+            <h3>Sign in</h3>
+    
+            <form action="">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Senha">
+            </form>
+    
+            <button id="signin-submit" class="modal-submit">REGISTRAR</button>
+        </div>
+    </dialog>
+    `
+    document.body.insertAdjacentHTML("beforeend", modalElementSignin);
+
+    // Lógica de abertura e fechamento do modal de signin
+    const modalSignin = document.querySelector('#dialog-signin')
+
+    const buttonSignin = document.querySelector('#signin-button')
+    buttonSignin.onclick = function () {
         modalLogin.close()
-    }
+        modalSignin.showModal()
+
+        const buttonClose = document.querySelector('#close-signin')
+        buttonClose.onclick = function () {
+            modalSignin.close()
+        }
+    }   
 });
