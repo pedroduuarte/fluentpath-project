a28c856b6b04f1f81d88fc4fe6028f5fdbd193b7
Daniel Ferreira <pocketrocket@outlook.com.br>
2025-02-12 21:52:54 -0300

    Criação do modal de login

--- Public/Scripts/header.js
+++ Public/Scripts/header.js
@@ -1,10 +1,43 @@
-window.addEventListener('scroll', changeHeaderColorOnScroll)
+document.addEventListener("DOMContentLoaded", () => {
+    // Troca a cor de fundo do header ao scrollar
+    window.addEventListener('scroll', () => {
+        const headerElement = document.querySelector('header')
+        if (window.scrollY > 0) {
+            headerElement.style.backgroundColor = 'rgba(9, 9, 9, 0.95)'
+        } else {
+            headerElement.style.backgroundColor = 'transparent'
+        }
+    })
 
-function changeHeaderColorOnScroll() {
-    const headerElement = document.querySelector('header')
-    if (window.scrollY > 0) {
-        headerElement.style.backgroundColor = 'rgba(9, 9, 9, 0.95)'
-    } else {
-        headerElement.style.backgroundColor = 'transparent'
+    // Criação do modal
+    const modalElement = `
+    <dialog>
+        <div id="form-login">
+            <button id="close"><img src="../Assets/close.png" alt=""></button>
+            <h3>Log in</h3>
+    
+            <form action="">
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Senha">
+            </form>
+    
+            <button id="login-submit">ENTRAR</button>
+            <button id="signin">Registre-se</button>
+        </div>
+    </dialog>
+    `
+    document.body.insertAdjacentHTML("beforeend", modalElement);
+
+    // Lógica de abertura e fechamento do modal de login
+    const modalLogin = document.querySelector('dialog')
+
+    const buttonLogin = document.querySelector('#loginButton')
+    buttonLogin.onclick = function () {
+        modalLogin.showModal()
     }
-}
+
+    const buttonClose = document.querySelector('#close')
+    buttonClose.onclick = function () {
+        modalLogin.close()
+    }
+});
